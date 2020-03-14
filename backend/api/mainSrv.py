f5c563e482ebbd3de2b867a4c51f87c1d9ef151b
florianschleuss <florian.schleuss@gmx.net>
2020-03-14 16:35:22 +0000

    Added restful api and MongoDB
    Signed-off-by: florianschleuss <florian.schleuss@gmx.net>

---
+++ backend/api/mainSrv.py
@@ -0,0 +1,30 @@
+import logging
+
+from pymongo import MongoClient
+from flask import Flask, jsonify, request
+from flask_httpauth import HTTPBasicAuth
+from flask_restful import Api
+
+app = Flask(__name__)
+api = Api(app)
+auth = HTTPBasicAuth()
+
+client = MongoClient('mongodb://florian:password@mongo-db:27017/lecture')
+
+@app.route('/api/v1/test', methods=['GET'])
+def test() -> int:
+    try:
+        logging.warning(client.test.list_collection_names())
+    except Exception:
+        logging.warning("fail")
+
+    db = client.lecture
+    semester = db.semseter
+    semester.delete_many({'test': 'blablabla'})
+    for elem in semester.find():
+        logging.warning(elem)
+    return jsonify({'message': 'semester.find()'})
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80, debug=True)
