3a166dae530ddab610b4fce2520ce69ea7615269
Daniel Schworm <danielschworm@t-online.de>
2020-03-19 22:23:50 +0100

    Added middleware functions
    Signed-off-by: Daniel Schworm <danielschworm@t-online.de>

--- frontend/lecture-schedule-app/src/app/api-client.service.ts
+++ frontend/lecture-schedule-app/src/app/api-client.service.ts
@@ -4,100 +4,173 @@ import { Observable, of } from "rxjs";
 import { catchError } from "rxjs/operators";
 
 @Injectable({
-  providedIn: "root"
+    providedIn: "root"
 })
 export class ApiClientService {
-  authentificated: boolean = true;
+    authentificated: boolean = true;
 
-  private baseUrl = "http://data.home-webserver.de:3010/api/v1"; // URL to web api
+    private baseUrl = "http://data.home-webserver.de:3010/api/v1"; // URL to web api
 
-  private httpOptions = {
-    headers: new HttpHeaders({
-      "Content-Type": "application/json"
-    })
-  };
-
-  constructor(private http: HttpClient) {}
-
-  private handleError<T>(result?: T) {
-    return (error: any): Observable<T> => {
-      console.error(error);
-      return of(result as T);
+    private httpOptions = {
+        headers: new HttpHeaders({
+            "Content-Type": "application/json"
+        })
     };
-  }
 
-  auth(email, psw): Observable<JSON> {
-    let authStr: string = email + ":" + psw;
-    return this.http.get<JSON>(this.baseUrl.concat("/authentification"), {
-      headers: new HttpHeaders({ Authorization: "Basic" + authStr })
-    });
-  }
+    constructor(private http: HttpClient) { }
 
-  ifAuthenticated(): boolean {
-    return this.authentificated;
-  }
+    private handleError<T>(result?: T) {
+        return (error: any): Observable<T> => {
+            console.error(error);
+            return of(result as T);
+        };
+    }
 
-  getUser(userId: string): Observable<JSON> {
-    return this.http.get<JSON>(this.baseUrl.concat("/users/").concat(userId));
-  }
+    auth(email, psw): Observable<JSON> {
+        let authStr: string = email + ":" + psw;
+        return this.http.get<JSON>(this.baseUrl.concat("/authentification"), {
+            headers: new HttpHeaders({ Authorization: "Basic" + authStr })
+        });
+    }
 
-  authUser(email: string, password: string): Observable<string> {
-    this.authentificated = true;
-    return this.http.post<string>(this.baseUrl.concat("/users"), {
-      "first-name": "firstName",
-      "last-name": "lastName",
-      "e-mail": email,
-      password: password
-    });
-  }
+    ifAuthenticated(): boolean {
+        return this.authentificated;
+    }
 
-  addUser(
-    firstName: string,
-    lastName: string,
-    email: string,
-    password: string
-  ): Observable<string> {
-    this.authentificated = true;
-    return this.http.post<string>(this.baseUrl.concat("/users"), {
-      "first-name": firstName,
-      "last-name": lastName,
-      "e-mail": email,
-      password: password
-    });
-  }
+    getUser(userId: string): Observable<JSON> {
+        return this.http.get<JSON>(this.baseUrl.concat("/users/").concat(userId));
+    }
 
-  addLecture(name: string, course: string, exam: string): Observable<string> {
-    this.authentificated = true;
-    return this.http.post<string>(this.baseUrl.concat("/lectures"), {
-      name: name,
-      course: course,
-      exam: exam
-    });
-  }
+    authUser(email: string, password: string): Observable<string> {
+        this.authentificated = true;
+        return this.http.post<string>(this.baseUrl.concat("/users"), {
+            "first-name": "firstName",
+            "last-name": "lastName",
+            "e-mail": email,
+            password: password
+        });
+    }
 
-  getLecture(lectureId: string): Observable<JSON> {
-    return this.http.get<JSON>(
-      this.baseUrl.concat("/lectures/").concat(lectureId)
-    );
-  }
+    addUser(
+        firstName: string,
+        lastName: string,
+        email: string,
+        password: string
+    ): Observable<string> {
+        this.authentificated = true;
+        return this.http.post<string>(this.baseUrl.concat("/users"), {
+            "first-name": firstName,
+            "last-name": lastName,
+            "e-mail": email,
+            password: password
+        });
+    }
 
-  // getList(id: string): Observable<List> {
-  //     if (id == undefined) { return; }
-  //     return this.http.get<List>(this.listUrl.concat(id))
-  //         .pipe(catchError(this.handleError<List>(new List()))
-  //         );
-  // };
+    getLectures(userId: string): Observable<JSON> {
+        return this.http.get<JSON>(
+            this.baseUrl
+                .concat("/users/")
+                .concat(userId)
+                .concat("/lectures")
+        );
+    }
 
-  // remItem(id: string, itemId: string): Observable<List> {
-  //     if (id == undefined) { return; }
-  //     if (itemId == undefined) { return; }
-  //     return this.http.delete<List>(this.listUrl.concat(id).concat('/items/').concat(itemId))
-  //         .pipe(catchError(this.handleError<List>(new List())));
-  // };
+    addLecture(
+        userId: string,
+        name: string,
+        course: string,
+        exam: string | null
+    ): Observable<string> {
+        this.authentificated = true;
+        return this.http.post<string>(
+            this.baseUrl
+                .concat("/users/")
+                .concat(userId)
+                .concat("/lectures"),
+            {
+                name: name,
+                course: course,
+                exam: exam
+            }
+        );
+    }
 
-  // updateItem(id: string, itemId: string, bought: boolean): Observable<List> {
-  //     if (id == undefined) { return; }
-  //     if (itemId == undefined) { return; }
-  //     return this.http.put<List>(this.listUrl.concat(id).concat('/items/').concat(itemId), { bought: bought })
-  // };
+    getLecture(userId: string, lectureId: string): Observable<JSON> {
+        return this.http.get<JSON>(
+            this.baseUrl
+                .concat("/users/")
+                .concat(userId)
+                .concat("/lectures/")
+                .concat(lectureId)
+        );
+    }
+
+    getDates(userId: string, lectureId: string): Observable<JSON> {
+        return this.http.get<JSON>(
+            this.baseUrl
+                .concat("/users/")
+                .concat(userId)
+                .concat("/lectures/")
+                .concat(lectureId)
+                .concat("/dates"),
+        );
+    }
+
+    addDate(
+        userId: string,
+        lectureId: string,
+        date: string,
+        morning: boolean,
+        room: string
+    ): Observable<JSON> {
+        return this.http.post<JSON>(
+            this.baseUrl
+                .concat("/users/")
+                .concat(userId)
+                .concat("/lectures/")
+                .concat(lectureId)
+                .concat("/dates"),
+            {
+                date: date,
+                morning: morning,
+                room: room
+            }
+        );
+    }
+
+    updateDate(
+        userId: string,
+        lectureId: string,
+        dateId: string,
+        date?: string,
+        morning?: boolean,
+        room?: string,
+    ): Observable<JSON> {
+        return this.http.patch<JSON>(
+            this.baseUrl
+                .concat("/users/")
+                .concat(userId)
+                .concat("/lectures/")
+                .concat(lectureId)
+                .concat("/dates/")
+                .concat(dateId),
+            {
+                date: date ? date : null,
+                moring: morning ? morning : null,
+                room: room ? room : null
+            }
+        );
+    }
+
+    remDate(userId: string, lectureId: string, dateId: string): Observable<JSON> {
+        return this.http.delete<JSON>(
+            this.baseUrl
+                .concat("/users/")
+                .concat(userId)
+                .concat("/lectures/")
+                .concat(lectureId)
+                .concat("/dates/")
+                .concat(dateId)
+        );
+    }
 }
